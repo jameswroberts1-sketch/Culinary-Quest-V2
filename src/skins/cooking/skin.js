819d672658d8c15c3e06fc6db86381f145ba7e74
jameswroberts1-sketch <jameswroberts1@outlook.com>
2025-11-13 22:01:28 +0000

    Update skin.js

--- src/skins/cooking/skin.js
+++ src/skins/cooking/skin.js
@@ -8,7 +8,7 @@ export const skin = {
   hydrateBrand(root) {
     const img = root.querySelector(".brand__logo");
     // IMPORTANT: path relative to THIS file’s folder
-    if (img) img.src = "./assets/CQ%20Logo.png";
+    if (img) img.src = "./src/skins/cooking/assets/CQ%20Logo.png";
   },
   headerHTML() {
     return `
